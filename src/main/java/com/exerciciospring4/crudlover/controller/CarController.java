e6f1f900f63117d9f36b5e2e0b4c6d7e6f65635a
RobinsonXavier <robinson.webzen@gmail.com>
2023-04-03 22:21:07 -0300

    Added update and delete car in CarController

--- src/main/java/com/exerciciospring4/crudlover/controller/CarController.java
+++ src/main/java/com/exerciciospring4/crudlover/controller/CarController.java
@@ -3,8 +3,11 @@ package com.exerciciospring4.crudlover.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +33,23 @@ public class CarController {
   @GetMapping
   public List<Car> listAll() {
     return repository.findAll();
+  }
+
+  @PutMapping("/{id}")
+  public void updateCar(@PathVariable Long id, @RequestBody @Valid CarDTO req) {
+    repository.findById(id).map( car -> {
+      car.setModelo(req.modelo());
+      car.setFabricante(req.fabricante());
+      car.setDataFabricacao(req.dataFabricacao());
+      car.setValor(req.valor());
+      car.setAnoModelo(req.anoModelo());
+      return repository.save(car);
+    });
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteCar(@PathVariable Long id) {
+    repository.deleteById(id);
   }
 
 }
